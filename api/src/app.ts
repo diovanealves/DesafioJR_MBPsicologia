21d6dfbb52a7fd1c96d98ec4bee4992b72a6dcf0
Diovane Alves <alvdiovane@gmail.com>
2023-12-29 20:03:22 -0300

    feat: add Cors dependency and configuration

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,10 @@
+import cors from 'cors'
 import express from 'express'
 import DisciplineRoutes from './routes/disciplineRoutes'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use(DisciplineRoutes)
 
