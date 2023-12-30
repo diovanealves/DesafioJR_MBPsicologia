0f9efb7b1eee036339854f02c13ce6262658f2be
Diovane Alves <alvdiovane@gmail.com>
2023-12-29 21:34:23 -0300

    feat: implement discipline type

---
+++ web/src/mocks/discipline.ts
@@ -0,0 +1,8 @@
+const disciplines = [
+  { title: 'Biologia', background: 'biology' },
+  { title: 'Artes', background: 'art' },
+  { title: 'Geografia', background: 'geography' },
+  { title: 'Sociologia', background: 'sociology' },
+]
+
+export default disciplines
