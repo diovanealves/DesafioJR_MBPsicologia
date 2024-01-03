2c1290e0b7edd0a83c267de68decfd8c643fe8dc
Diovane Alves <alvdiovane@gmail.com>
2024-01-03 16:25:38 -0300

    refactor: add toast component to the hook

--- web/src/hooks/useApi.ts
+++ web/src/hooks/useApi.ts
@@ -1,44 +1,30 @@
-import { useToast } from '@/components/ui/use-toast'
+import ShowMyToast from '@/components/ShowMyToast'
 import { DisciplineData } from '@/schemas/NoteData'
 import apiService from '@/services/apiService'
+import { isAxiosError } from 'axios'
+import { useCallback } from 'react'
 
 export default function UseApi() {
-  const { toast } = useToast()
-
-  async function createNote(data: DisciplineData) {
+  const createNote = useCallback(async (data: DisciplineData) => {
     try {
       const response = await apiService.createNote(data)
 
-      if (response.status === 201) {
-        toast({
-          description: 'Nota criada com sucesso',
-          variant: 'default',
-          duration: 4500,
-        })
-        return response.data
-      } else {
-        toast({
-          description: 'Erro ao criar. Tente novamente mais tarde.',
-          variant: 'destructive',
-          duration: 4500,
-        })
+      if (response.status !== 201) {
+        ShowMyToast('Erro ao criar. Tente novamente mais tarde.', 'destructive')
       }
-    } catch (error: any) {
-      if (error.response && error.response.data) {
-        toast({
-          description: error.response.data.error,
-          variant: 'destructive',
-          duration: 4500,
-        })
-      } else {
-        toast({
-          description: 'Erro desconhecido. Por favor, tente novamente.',
-          variant: 'destructive',
-          duration: 4500,
-        })
-      }
+
+      ShowMyToast('Nota criado com sucesso.', 'default')
+      return response.data
+    } catch (error) {
+      const isAxiosErr = isAxiosError(error)
+      const errorMessage =
+        isAxiosErr && error.response?.data.error
+          ? error.response?.data.error
+          : 'Erro desconhecido. Por favor, tente novamente.'
+
+      ShowMyToast(errorMessage, 'destructive')
     }
-  }
+  }, [])
 
   return { createNote }
 }
