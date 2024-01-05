b5bc9a31180d1696e0592dc654aceea0290ff57d
Diovane Alves <alvdiovane@gmail.com>
2024-01-05 12:41:26 -0300

    feat: map backend data and display to user

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,12 +1,13 @@
 import MyDialog from '@/components/MyDialog'
 import NoteCard from '@/components/NoteCard'
-import UseNote from '@/hooks/useNote'
+import useNote from '@/hooks/useNote'
+import { AlertTriangle } from 'lucide-react'
 
 export default function Home() {
-  const { noteByBimester } = UseNote()
+  const { noteByBimester } = useNote()
 
   return (
-    <div className="px-5 py-32">
+    <div className="px-5 py-10">
       {noteByBimester.map((bimester, i) => (
         <div key={i}>
           <div className="flex items-center justify-between">
@@ -17,10 +18,25 @@ export default function Home() {
             />
           </div>
 
-          <div className="grid grid-cols-2 gap-2 my-6">
-            {bimester.notes.map((notes, index) => (
-              <NoteCard key={index} variant={notes.disciplina} {...notes} />
-            ))}
+          <div
+            className={`my-6 ${
+              bimester.notes.length > 0
+                ? 'grid grid-cols-2 gap-2 '
+                : 'text-center'
+            }`}
+          >
+            {bimester.notes.length > 0 ? (
+              bimester.notes.map((notes) => (
+                <NoteCard key={notes.id} {...notes} />
+              ))
+            ) : (
+              <div className="flex justify-center items-center gap-2">
+                <AlertTriangle className="w-10 h-10 text-red-500" />
+                <h1 className="font-medium">
+                  Ainda não tem nenhuma nota cadastrada
+                </h1>
+              </div>
+            )}
           </div>
         </div>
       ))}
